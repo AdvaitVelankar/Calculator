eb3ad7630d907646412b0bade70db99efcb81598
AdvaitVelankar <108892478+AdvaitVelankar@users.noreply.github.com>
2022-07-10 21:50:51 +0530

    Add files via upload

---
+++ Normal Calculator.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Normal_Calculator
+{
+    public static void main(String args[])
+    {
+        double a, b, ans;
+        char ch;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a: ");
+        a=sc.nextDouble();
+        System.out.println("Enter the value of b: ");
+        b=sc.nextDouble();
+        System.out.println("What do you want to do");
+        System.out.println("For Addition press 1");
+        System.out.println("For Subtraction press 2");
+        System.out.println("For Multiplication press 3");
+        System.out.println("For Division press 4");
+        System.out.println("For Remainter press 5");
+        ch=sc.next().charAt(0);
+        switch(ch)
+        {
+            case '1': ans=(a+b);
+            System.out.println("Your Addition is:" +ans);
+            break;
+            case '2': ans=(a-b);
+            System.out.println("Your Subtraction is:" +ans);
+            break;
+            case '3': ans=(a*b);
+            System.out.println("Your Multiplication is:" +ans);
+            break;
+            case '4': ans=(a/b);
+            System.out.println("Your Division is:" +ans);
+            break;
+            case '5': ans=(a%b);
+            System.out.println("Your Remainder is:" +ans);
+            default:System.out.println("ERROR");
+        }
+    }
+}
