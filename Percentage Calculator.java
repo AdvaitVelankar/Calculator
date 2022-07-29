cf6fb8a8bae728aa4bad847de83cd3b281a89bce
AdvaitVelankar <108892478+AdvaitVelankar@users.noreply.github.com>
2022-07-29 19:58:53 +0530

    Update Percentage Calculator.java

--- Percentage Calculator.java
+++ Percentage Calculator.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Percentage_Calculator
 {
-    public static void main()
+    public static void main(String args[])
     {
         for(char z=1; z<=2; z++)
         {
